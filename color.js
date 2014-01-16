91693b3a4b756ae038d14195c9194bf80516313f
Rob Becker <rob.becker@webfilings.com>
2014-01-15 19:57:46 -0700

    Examples added

---
+++ color.js
@@ -0,0 +1,12 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/cu.Sphero-RGY-RN-SPP' },
+  device: { name: 'sphero', driver: 'sphero' },
+
+  work: function(me) {
+    every((1).second(), function() {
+      me.sphero.setRGB(Math.floor(Math.random() * 100000));
+    });
+  }
+}).start();
